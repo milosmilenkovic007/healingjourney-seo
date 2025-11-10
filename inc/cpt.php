dfc14375e8d4374bf20d7b25603ddd8ea4b03af7
Milos <mm@webnorth.dk>
2025-11-10 22:59:22 +0100

    Ensure saving seo_site returns to its edit screen (redirect_post_location)

--- inc/cpt.php
+++ inc/cpt.php
@@ -42,3 +42,13 @@ add_action('init', function() {
     'rewrite' => ['slug' => 'content-plan'],
   ]);
 });
+
+// Force redirect back to the seo_site edit screen after save (avoid falling back to Posts list)
+add_filter('redirect_post_location', function($location, $post_id){
+  $post = get_post($post_id);
+  if ($post && $post->post_type === 'seo_site') {
+    // Ensure we're returning to the edit screen for this post type
+    $location = add_query_arg(['post' => $post_id, 'action' => 'edit'], admin_url('post.php'));
+  }
+  return $location;
+}, 10, 2);
